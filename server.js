4870271b2653d48a17fcd733200276d7058bb264
Gabe Sucich <gabe.sucich@gmail.com>
2021-04-01 23:10:57 -0700

    Debugging deployment

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
+var path = require('path')
 var express = require('express')
 var session = require("express-session");
 var db = require("./models")
 var passport = require('./config/passport')
+
+var publicPath = path.join(__dirname, "client", "public")
 
 var PORT = process.env.PORT || 5000;
 
@@ -9,6 +12,8 @@ app = express()
 
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
+
+app.use(express.static(publicPath))
 
 
 app.use(session({ secret: "True_count_is_4", resave: true, saveUninitialized: true }));
